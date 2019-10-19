b0afb328dd39c545af3f252d2a9a44cad2e7a238
Ольга <olga.lucky7@gmail.com>
2019-10-20 00:42:11 +0300

    added new column user_id to table posts

---
+++ database/migrations/2019_10_19_213933_add_user_id_column_to_posts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserIdColumnToPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            //php artisan make:migration add_user_id_column_to_posts_table --table=posts
+            $table->unsignedBigInteger('user_id')->after('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            //
+        });
+    }
+}
